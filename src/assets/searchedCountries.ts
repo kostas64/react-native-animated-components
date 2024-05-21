68d09b5a1c6075bc6dd0f5501599289dde92ccd2
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-05-21 15:20:36 +0300

    assets for airbnb

---
+++ src/assets/searchedCountries.ts
@@ -0,0 +1,57 @@
+export const SEARCH_COUNTRIES = [
+  {
+    place: 'Jordan',
+    guests: 2,
+    date: '10-14 Jul',
+  },
+  {
+    place: 'Lisbon',
+    guests: 2,
+    date: '14-18 Jul',
+  },
+  {
+    place: 'Paris',
+    guests: 4,
+    date: '06-14 Aug',
+  },
+  {
+    place: 'Belgium',
+    guests: 3,
+    date: '01-10 Sep',
+  },
+  {
+    place: 'Los Angeles',
+    guests: 3,
+    date: '19-24 Sep',
+  },
+  {
+    place: 'Chicago',
+    guests: 3,
+    date: '02-10 Oct',
+  },
+  {
+    place: 'Thessaloniki',
+    guests: 3,
+    date: '11-20 Oct',
+  },
+  {
+    place: 'Corfu',
+    guests: 3,
+    date: '21-24 Oct',
+  },
+  {
+    place: 'Los Angeles',
+    guests: 3,
+    date: '20-25 Dec',
+  },
+  {
+    place: 'Athens',
+    guests: 6,
+    date: '25-28 Dec',
+  },
+  {
+    place: 'USA',
+    guests: 6,
+    date: '28-30 Dec',
+  },
+];
