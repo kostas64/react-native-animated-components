0b4f540ae92bcd85cfa0f4b7845b5fe31692b8e2
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-08-27 12:45:16 +0300

    new fonts

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,15 +1,8 @@
-import {isIOS} from './device';
-
 export const typography = {
-  thin: isIOS ? 'FSP DEMO - Facundo Thin Regular' : 'Fontspring-Thin',
-  semiBold: isIOS
-    ? 'FSP DEMO - Facundo SemiBold Regular'
-    : 'Fontspring-SemiBold',
-  regular: isIOS ? 'FSP DEMO - Facundo Regular' : 'Fontspring-Regular',
-  light: isIOS ? 'FSP DEMO - Facundo Light Regular' : 'Fontspring-Light',
-  extraLight: isIOS
-    ? 'FSP DEMO - Facundo ExtraLight Regular'
-    : 'Fontspring-ExtraLight',
-  bold: isIOS ? 'FSP DEMO - Facundo Bold Regular' : 'Fontspring-Bold',
-  black: isIOS ? 'FSP DEMO - Facundo Black Regular' : 'Fontspring-Black',
+  extraBold: 'Montserrat-ExtraBold',
+  bold: 'Montserrat-Bold',
+  semiBold: 'Montserrat-SemiBold',
+  regular: 'Montserrat-Regular',
+  light: 'Montserrat-Light',
+  medium: 'Montserrat-Medium',
 };
