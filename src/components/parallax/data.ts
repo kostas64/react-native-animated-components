df92ebe5fa8daee578c463a2a6bc0d88128c5a9c
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-09-08 18:39:55 +0300

    componentization parallaxlist

---
+++ src/components/parallax/data.ts
@@ -0,0 +1,9 @@
+import images from '../../assets/parallaxList';
+
+export const data = images.map((image, index) => ({
+  key: String(index),
+  photo: image,
+  avatar_url: `https://randomuser.me/api/portraits/women/${Math.floor(
+    Math.random() * 40,
+  )}.jpg`,
+}));
