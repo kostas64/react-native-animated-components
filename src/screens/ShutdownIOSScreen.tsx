dcf41e0675a6c13b059881e25407e0c5100b5fb0
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-09-14 14:23:35 +0300

    Refactor animation to not use react-native-linear-gradient

--- src/screens/ShutdownIOSScreen.tsx
+++ src/screens/ShutdownIOSScreen.tsx
@@ -3,24 +3,25 @@ import Animated, {
   withRepeat,
   interpolate,
   useSharedValue,
-  interpolateColor,
   useAnimatedStyle,
+  useAnimatedProps,
 } from 'react-native-reanimated';
 import React from 'react';
 import Feather from 'react-native-vector-icons/Feather';
-import LinearGradient from 'react-native-linear-gradient';
 import {View, StyleSheet, ImageBackground} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+import Svg, {Defs, LinearGradient, Stop, Rect} from 'react-native-svg';
 
 import {HEIGHT_SCR, WIDTH} from '@utils/device';
 import StatusBarManager from '@components/StatusBarManager';
 import AnimatedText from '@components/shutdownIOS/AnimatedText';
 import {SLIDER_FINAL_W, SLIDER_H, SLIDER_W} from '@components/shutdownIOS/data';
 
-const AnimLinear = Animated.createAnimatedComponent(LinearGradient);
+const AnimSvg = Animated.createAnimatedComponent(Svg);
+const AnimRect = Animated.createAnimatedComponent(Rect);
 
 const ShutdownIOS = () => {
-  const silderWidth = useSharedValue(SLIDER_W);
+  const sliderWidth = useSharedValue(SLIDER_W);
   const coloring = useSharedValue(0);
   const reachEnd = useSharedValue(false);
   const finishProgress = useSharedValue(0);
@@ -28,10 +29,11 @@ const ShutdownIOS = () => {
   const chars = 'slide to power off'.split('');
   const totalCharsLength = chars.length;
 
+  // Animated background opacity for slider
   const background = useAnimatedStyle(
     () => ({
       opacity: interpolate(
-        silderWidth.value,
+        sliderWidth.value,
         [SLIDER_W, SLIDER_FINAL_W],
         [0.75, 1],
       ),
@@ -39,56 +41,52 @@ const ShutdownIOS = () => {
     [],
   );
 
-  const innerBackground = useAnimatedStyle(() => ({
-    backgroundColor: interpolateColor(
-      silderWidth.value,
-      [SLIDER_W, 75],
-      ['rgba(0,0,0,0)', 'rgba(0,0,0,1)'],
-    ),
-    width: silderWidth.value,
+  const animProps = useAnimatedProps(() => ({
+    width: sliderWidth.value,
+    opacity: interpolate(sliderWidth.value, [SLIDER_W, 75], [1, 0]),
+
+    transform: [{translateX: SLIDER_W - sliderWidth.value}],
   }));
 
-  const slider = useAnimatedStyle(
-    () => ({
-      width: silderWidth.value,
-      transform: [{translateX: 268 - silderWidth.value}],
-    }),
-    [],
-  );
-
+  // Power button animation
   const powerBtn = useAnimatedStyle(() => ({
     opacity: interpolate(finishProgress.value, [0, 1], [1, 0]),
     transform: [
       {scale: interpolate(finishProgress.value, [0, 0.2, 1], [1, 1.2, 0])},
     ],
+    left: interpolate(sliderWidth.value, [SLIDER_W, SLIDER_FINAL_W], [3, 195]),
   }));
 
+  // Pan gesture for sliding the slider
   const gesture = Gesture.Pan()
     .onChange(e => {
       if (e.translationX < 0) {
-        //Case user swipe left
-        silderWidth.value = SLIDER_W;
+        // Reset when swiping left
+        sliderWidth.value = SLIDER_W;
       } else if (SLIDER_W - e.translationX > SLIDER_FINAL_W) {
-        //Case user swipe right and slider didnt reach end
-        silderWidth.value = SLIDER_W - e.translationX;
-      } else if (silderWidth.value < 90) {
-        //Trigger finish animation
+        sliderWidth.value = SLIDER_W - e.translationX;
+
+        if (reachEnd.value) {
+          reachEnd.value = false;
+        }
+      } else if (sliderWidth.value < 90) {
+        // Trigger finish animation
         reachEnd.value = true;
       }
     })
     .onFinalize(() => {
       if (reachEnd.value) {
-        //Finish animation - Reset when end
-        finishProgress.value = withTiming(1, {duration: 500}, finised => {
-          if (finised) {
-            silderWidth.value = withTiming(SLIDER_W);
+        finishProgress.value = withTiming(1, {duration: 500}, finished => {
+          if (finished) {
+            // Reset slider
+            sliderWidth.value = withTiming(SLIDER_W);
             reachEnd.value = false;
             finishProgress.value = withTiming(0);
           }
         });
       } else {
-        //Too fast and much sliding - Revert it
-        silderWidth.value = withTiming(SLIDER_W);
+        // Revert slider if not finished
+        sliderWidth.value = withTiming(SLIDER_W);
       }
     });
 
@@ -104,32 +102,44 @@ const ShutdownIOS = () => {
         style={{width: WIDTH, height: HEIGHT_SCR}}>
         <Animated.View style={[styles.filterBackground, background]} />
 
-        <Animated.View style={styles.sliderContainer}>
-          <AnimLinear
-            start={{x: 0.35, y: 0}}
-            colors={['#bf3354', '#f43f46', '#fe5334']}
-            style={[styles.radius200, slider]}>
-            <Animated.View style={[innerBackground, styles.sliderFilter]} />
-            <GestureDetector gesture={gesture}>
-              <Animated.View style={[powerBtn, styles.powerBtn]}>
-                <Feather name="power" size={36} color={'#da0b13'} />
-              </Animated.View>
-            </GestureDetector>
-
-            <View style={styles.textContainer}>
-              {chars.map((char, index) => (
-                <AnimatedText
-                  key={`index-${index}`}
-                  char={char}
-                  index={index}
-                  coloring={coloring}
-                  silderWidth={silderWidth}
-                  totalCharsLength={totalCharsLength}
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={styles.sliderContainer}>
+            <View style={styles.sliderInnerContainer}>
+              <AnimSvg width={SLIDER_W} height={SLIDER_H}>
+                <Defs>
+                  <LinearGradient id="grad" x1="0" y1="0" x2="1" y2="0">
+                    <Stop offset="0%" stopColor="#bf3354" />
+                    <Stop offset="50%" stopColor="#f43f46" />
+                    <Stop offset="100%" stopColor="#fe5334" />
+                  </LinearGradient>
+                </Defs>
+                <AnimRect
+                  x="0"
+                  y="0"
+                  height={SLIDER_H}
+                  rx="40"
+                  fill="url(#grad)"
+                  animatedProps={animProps}
                 />
-              ))}
+                <View style={styles.textContainer}>
+                  {chars.map((char, index) => (
+                    <AnimatedText
+                      key={`index-${index}`}
+                      char={char}
+                      index={index}
+                      coloring={coloring}
+                      sliderWidth={sliderWidth}
+                      totalCharsLength={totalCharsLength}
+                    />
+                  ))}
+                </View>
+              </AnimSvg>
             </View>
-          </AnimLinear>
-        </Animated.View>
+            <Animated.View style={[powerBtn, styles.powerBtn]}>
+              <Feather name="power" size={36} color={'#da0b13'} />
+            </Animated.View>
+          </Animated.View>
+        </GestureDetector>
       </ImageBackground>
     </>
   );
@@ -148,21 +158,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     left: (WIDTH - SLIDER_W) / 2,
     marginTop: 150,
-    height: SLIDER_H,
+    height: SLIDER_H + 10,
+    overflow: 'hidden',
   },
-  sliderFilter: {
-    position: 'absolute',
-    borderRadius: 200,
-    height: SLIDER_H,
-    width: SLIDER_W,
+  sliderInnerContainer: {
+    position: 'relative',
+    overflow: 'hidden',
+    top: 5,
   },
   powerBtn: {
     position: 'absolute',
     padding: 16,
     backgroundColor: 'white',
     borderRadius: 100,
-    top: 4,
-    left: 3,
+    top: 9,
   },
   textContainer: {
     flexDirection: 'row',
@@ -170,8 +179,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     top: 28,
     left: 68,
-  },
-  radius200: {
-    borderRadius: 200,
+    zIndex: 100000,
   },
 });
