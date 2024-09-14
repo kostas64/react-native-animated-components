e6365f44bd4ba239761129c904175131a7a8d283
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-09-14 14:46:10 +0300

    refactor value pickers to use linear-gradient from svg

--- src/screens/ValuePickersScreen.tsx
+++ src/screens/ValuePickersScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
+import Svg, {Defs, LinearGradient, Stop, Rect} from 'react-native-svg';
 
 import ValueDotPicker from '@components/valuePickers/valueDot/ValueDotPicker';
 import ValueListPicker from '@components/valuePickers/valueList/ValueListPicker';
@@ -18,7 +18,7 @@ type TPickerContainer = {
 };
 
 const PickerContainer = ({children}: TPickerContainer) => {
-  return <View style={styles.pickerContainer}>{children}</View>;
+  return <View style={styles.pickerWrapper}>{children}</View>;
 };
 
 const ValuePickersScreen = () => {
@@ -27,57 +27,68 @@ const ValuePickersScreen = () => {
   const [arrowListValue, setArrowListValue] = React.useState(0);
 
   return (
-    <LinearGradient
-      style={styles.container}
-      colors={['#c4e4e1', '#3b5998', '#c4e4e1']}>
-      <View style={styles.rowAround}>
-        <PickerContainer>
-          <ValueDotPicker
-            unit="°"
-            range={[0, 15]}
-            value={dotValue}
-            setValue={setDotValue}
-          />
-        </PickerContainer>
-        <PickerContainer>
-          <ValueListPicker
-            unit="°"
-            value={listValue}
-            range={[-28, -10]}
-            setValue={setListValue}
-          />
-        </PickerContainer>
-        <PickerContainer>
-          <ValueArrowPicker
-            unit="°"
-            range={[-28, -10]}
-            value={arrowListValue}
-            setValue={setArrowListValue}
-          />
-        </PickerContainer>
+    <>
+      <Svg width={'100%'} height={'100%'}>
+        <Defs>
+          <LinearGradient id="grad" x1="0" y1="1" x2="0" y2="0">
+            <Stop offset="0%" stopColor="#c4e4e1" />
+            <Stop offset="50%" stopColor="#3b5998" />
+            <Stop offset="100%" stopColor="#c4e4e1" />
+          </LinearGradient>
+        </Defs>
+        <Rect x="0" y="0" height={'100%'} width={'100%'} fill="url(#grad)" />
+      </Svg>
+      <View style={styles.pickersContainer}>
+        <View style={styles.rowAround}>
+          <PickerContainer>
+            <ValueDotPicker
+              unit="°"
+              range={[0, 15]}
+              value={dotValue}
+              setValue={setDotValue}
+            />
+          </PickerContainer>
+          <PickerContainer>
+            <ValueListPicker
+              unit="°"
+              value={listValue}
+              range={[-28, -10]}
+              setValue={setListValue}
+            />
+          </PickerContainer>
+          <PickerContainer>
+            <ValueArrowPicker
+              unit="°"
+              range={[-28, -10]}
+              value={arrowListValue}
+              setValue={setArrowListValue}
+            />
+          </PickerContainer>
+        </View>
       </View>
-    </LinearGradient>
+    </>
   );
 };
 
 export default ValuePickersScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
+  pickersContainer: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
     justifyContent: 'center',
-    backgroundColor: '#a1a1a1',
   },
   rowAround: {
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-  pickerContainer: {
+  pickerWrapper: {
     width: 64,
     height: 200,
-    borderRadius: 26,
+    borderRadius: 20,
     backgroundColor: '#161616',
+    zIndex: 100000,
   },
 });
