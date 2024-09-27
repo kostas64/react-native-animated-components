6bd6b7dc6a0c8d5058053bcd980b2dc883027adc
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-09-27 09:39:38 +0300

    Add buttons to animate charts

--- src/screens/GroupStackCharts.tsx
+++ src/screens/GroupStackCharts.tsx
@@ -1,29 +1,35 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {Easing, useSharedValue, withTiming} from 'react-native-reanimated';
 
+import MyButton from '@components/charts/MyButton';
 import GroupChart from '@components/charts/groupChart/GroupChart';
 import StackChart from '@components/charts/stackChart/StackChart';
 
 const GroupStackCharts = () => {
-  const navigation = useNavigation();
-
+  const insets = useSafeAreaInsets();
   const animate = useSharedValue(0);
 
-  useEffect(() => {
-    //@ts-ignore
-    const listener = navigation.addListener('transitionEnd', () => {
-      animate.value = withTiming(1, {duration: 1500, easing: Easing.ease});
-    });
+  const paddingTop = insets.top > 0 ? insets.top + 8 : 24;
+  const paddingBottom = insets.bottom > 0 ? insets.bottom + 8 : 24;
 
-    return () => !!listener && listener();
-  });
+  const animateIt = (forward = true) => {
+    const toValue = forward ? 1 : 0;
+    animate.value = withTiming(toValue, {duration: 1000, easing: Easing.ease});
+  };
 
   return (
-    <View style={styles.container}>
-      <GroupChart animate={animate} />
-      <StackChart animate={animate} />
+    <View style={[styles.container, {paddingTop, paddingBottom}]}>
+      <View style={styles.chartsContainer}>
+        <GroupChart animate={animate} />
+        <StackChart animate={animate} />
+      </View>
+
+      <View style={styles.btnsContainer}>
+        <MyButton title="Animate" onPress={animateIt} />
+        <MyButton title="Animate Back" onPress={() => animateIt(false)} />
+      </View>
     </View>
   );
 };
@@ -36,5 +42,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#eeeee2',
     justifyContent: 'center',
+  },
+  chartsContainer: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+  btnsContainer: {
+    justifyContent: 'space-around',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 52,
+    marginTop: 16,
   },
 });
