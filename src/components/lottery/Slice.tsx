3139d412d80a05f83d83f258b4e8069e4a6d97fb
Konstantinos Efkarpidis <kostas11062@gmail.com>
2024-12-21 18:44:44 +0200

    performance improvement

--- src/components/lottery/Slice.tsx
+++ src/components/lottery/Slice.tsx
@@ -1,73 +1,73 @@
-import React from 'react';
 import {Path, Text} from 'react-native-svg';
+import React, {memo, useCallback} from 'react';
 
 import {TListItem, TSlice} from './types';
 import {typography} from '@utils/typography';
+import {OUTER_BORDER_W, RADIUS} from './constants';
 import {calculateTextPosition, createSlicePath} from './utils';
-import {BORDER_WIDTH, OUTER_BORDER_W, RADIUS} from './constants';
 
-const Slice = ({
-  item,
-  index,
-  isSelected,
-  total,
-  spinning,
-  selectOption,
-}: TSlice & TListItem) => {
-  const startAngle = total === 1 ? 0 : (360 / total) * index;
-  const endAngle = total === 1 ? 360 : startAngle + 360 / total;
+const Slice = memo(
+  ({
+    item,
+    index,
+    isSelected,
+    total,
+    spinning,
+    selectOption,
+  }: TSlice & TListItem) => {
+    const startAngle = total === 1 ? 0 : (360 / total) * index;
+    const endAngle = total === 1 ? 360 : startAngle + 360 / total;
 
-  const path = createSlicePath(
-    startAngle,
-    endAngle,
-    RADIUS - OUTER_BORDER_W / 2,
-  );
+    const path = createSlicePath(
+      startAngle,
+      endAngle,
+      RADIUS - OUTER_BORDER_W / 2,
+    );
 
-  const fill = isSelected
-    ? 'url(#centerCircle)'
-    : index % 2 === 0
-    ? 'url(#sliceGradint)'
-    : '#7a54cd';
+    const fill = isSelected
+      ? 'url(#centerCircle)'
+      : index % 2 === 0
+      ? 'url(#sliceGradint)'
+      : '#7a54cd';
 
-  const {x, y, angle} = calculateTextPosition(startAngle, endAngle, RADIUS);
+    const {x, y, angle} = calculateTextPosition(startAngle, endAngle, RADIUS);
 
-  const onPress = () => {
-    if (spinning.value) {
-      return;
-    }
+    const onPress = useCallback(() => {
+      if (spinning.value) {
+        return;
+      }
 
-    selectOption(index);
-  };
+      selectOption(index);
+    }, []);
 
-  return (
-    <React.Fragment>
-      {/* Slice */}
-      <Path
-        d={path}
-        fill={fill}
-        x={OUTER_BORDER_W / 2}
-        y={OUTER_BORDER_W / 2}
-        stroke="#fff"
-        onPressIn={onPress}
-        strokeWidth={BORDER_WIDTH}
-      />
+    return (
+      <React.Fragment>
+        {/* Slice */}
+        <Path
+          d={path}
+          fill={fill}
+          onPressIn={onPress}
+          x={OUTER_BORDER_W / 2}
+          y={OUTER_BORDER_W / 2}
+        />
 
-      {/* Slice Value */}
-      <Text
-        x={x + 16}
-        y={y + 4}
-        fontSize={38}
-        fill="#FFFFFF"
-        fontWeight={'700'}
-        textAnchor="middle"
-        alignmentBaseline="middle"
-        fontFamily={typography.bold}
-        onPress={onPress}
-        transform={`rotate(${angle}, ${x}, ${y})`}>
-        {item}
-      </Text>
-    </React.Fragment>
-  );
-};
+        {/* Slice Value */}
+        <Text
+          x={x + 16}
+          y={y + 4}
+          fontSize={38}
+          fill="#FFFFFF"
+          fontWeight={'700'}
+          textAnchor="middle"
+          alignmentBaseline="middle"
+          fontFamily={typography.bold}
+          onPress={onPress}
+          transform={`rotate(${angle}, ${x}, ${y})`}>
+          {item}
+        </Text>
+      </React.Fragment>
+    );
+  },
+);
 
 export default Slice;
